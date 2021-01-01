6bd736beab21298af5d5e80f60c3191131302988
Maly <migsking@naver.com>
2021-01-01 22:21:58 +0900

    init

---
+++ src/test/java/com/mallycrip/clean/CleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallycrip.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
